b8ee118d61859d189d1dc77aa39291cc06d9961b
ZainHanif29 <119613534+ZainHanif29@users.noreply.github.com>
2024-04-17 23:01:48 +0500

    setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <div className="container">
+    <div className="row">
+      <h1>EventSphere_Management</h1>
+    </div>
+   </div>
+   </>
+  )
+}
+
+export default App
