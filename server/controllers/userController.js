bb34daaf992b4c1afd1dadf054a6f97355d97c71
ZainHanif29 <zainhanif2902@gmail.com>
2024-04-19 14:38:28 +0500

    login functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,12 @@
 import UserModel from "../models/userModel.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 class userController {
+
+    // Rigester
     static userRigester = async (req, res) => {
-        const { FirstName, LastName, Email, Phone, Password, PasswordConfirm} = req.body;
+        const { FirstName, LastName, Email, Phone, Password, PasswordConfirm } = req.body;
         const user = await UserModel.findOne({ Email: Email });
         if (user) {
             res.json({ status: 'failed', message: 'user already exit' })
@@ -13,11 +16,15 @@ class userController {
                     try {
                         const hashPassword = await bcrypt.hash(Password, 10)
                         const doc = new UserModel({
-                            FirstName,LastName,Email,Phone,Password:hashPassword
+                            FirstName, LastName, Email, Phone, Password: hashPassword
                         })
                         await doc.save();
 
-                        res.json({ status: 'success', message: "User Rigester!!! 👍" })
+                        // Generate JWT Token
+                        const savedUser = await UserModel.findOne({Email:Email})
+                        const token = jwt.sign({userID:savedUser._id},process.env.JWT_TOKEN,{expiresIn:process.env.JWT_TOKEN_EXP})
+
+                        res.json({ status: 'success', message: "User Rigester!!! 👍" , 'token':token})
 
                     } catch (error) {
                         res.json({ status: 'failed', message: "User Not Rigester!!! 😢" })
@@ -29,6 +36,31 @@ class userController {
                 res.json({ status: 'failed', message: 'all filed are required' })
             }
         }
+    }
+
+    // Login
+    static userLogin = async (req, res) => {
+        const { Email, Password } = req.body;
+        if (Email && Password) {
+            const user = await UserModel.findOne({ Email: Email });
+            if (user) {
+                const password = await bcrypt.compare(Password,user.Password);
+                if((Email === emailCheck) && password){
+                    // Generate JWT Token
+                    const token = jwt.sign({userID:user._id},process.env.JWT_TOKEN,{expiresIn:process.env.JWT_TOKEN_EXP})
+                    res.json({status:"success",message:"login successfully!!! 👍",'token':token})
+                }else{
+                    res.json({status:"failed",message:"Email or Password is not valid! 😢"})
+                }
+            }else{
+                res.json({status:"failed",message:"User not Rigester  😒"})
+            }
+        }else{
+            res.json({status:"failed",message:"all field are required  😒"})
+
+        }
+
+
     }
 }
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 // Public Routes
 router.post('/rigester',userController.userRigester)
+router.post('/login',userController.userLogin)
 
 // Protect Routes
 
